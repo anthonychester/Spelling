03933b598e39c98e3e49ea2ee2f902a82b2a0f9a
anthonychester <anthony.chester@pcstudent.net>
2021-02-23 20:17:57 +0000

    Add text input slot for later use

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import "./styles.css";
+//import "./styles.css";
 
 var words = ["cow", "dog", "cat"];
 var ask = [];
@@ -9,27 +9,34 @@ for (var i = 0; i < words.length; i++) {
   if (rand === 0) {
     rand = rand + 1;
   }
+  if (rand === words[i].length) {
+    rand = rand - 1;
+  }
   var char = word.charAt(rand);
   word = word.replace(char, "_");
+  console.log(word);
 
   ask.unshift(word);
 }
 
-function askWord(str) {
-  var lo = str.search("_");
-  var p1 = str.slice(lo, 0);
-  var p2 = str.slice(0, lo);
-  //var out = "<form>";
+function askWord(strin) {
+  var lo = strin.search("_");
+  //console.log(lo);
+  var p1 = strin.slice(0, lo);
+  var p2 = strin.slice(lo + 1);
+  var out = "<form>";
   var out = out + p1;
-  //out = +'<input type="text" id="fname" name="fname"><br>';
+  out = out +'<input type="text" id="fname" name="fname" size="1">';
+  //out = out + " ";
   out = out + p2;
-  //out = out + `</form>`;
+  out = out + `</form>`;
+  console.log(p1);
   return out;
 }
 document.getElementById("app").innerHTML = ask + "<br>";
 
-for (i = 0; i > ask.length; i++) {
-  var qt = askWord(ask[i]);
+for (i = 0; i < ask.length; i++) {
+  qt = askWord(ask[i]);
   document.getElementById("test").innerHTML =
     document.getElementById("test").innerHTML + qt;
 
